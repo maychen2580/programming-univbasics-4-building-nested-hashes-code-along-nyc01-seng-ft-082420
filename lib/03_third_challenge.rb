59a35a962356a622367a10e44e54a251942eb24f
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-13 21:54:48 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -17,13 +17,15 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"
-        #your key/value pairs here
+        
         },
       :matriarch => {
         #your key/value pairs here
+        name: "Lady Capulet", age: "51"
         },
       :heroine => {
         #your key/value pairs here
+        name: "Juliet", age: "15", status: "alive"
         },
       :heroine_friends => []
    }
