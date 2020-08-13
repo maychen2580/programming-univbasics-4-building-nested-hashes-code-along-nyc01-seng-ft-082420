638c0487842771840148f4ecf20111e57ae04a11
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-13 21:45:13 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,18 +1,12 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
-       :patriarch => {}, 
-       :matriarch => {}, 
-       :hero =>{}, 
-       :hero_friends => []
        
      }, 
      :capulet => {
-       :patriarch =>{}, 
-       :matriarch => {},
-       :heroine => {},
-       :heroine_friends => []
-     }
+      
+      
      
+   }
    }
 end
