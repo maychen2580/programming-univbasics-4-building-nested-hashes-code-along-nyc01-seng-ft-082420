95bf7cb4652fbf2c523f1d273412b9e70beb9852
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-13 21:57:57 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -6,7 +6,8 @@ def third_challenge
         name: "Lord Montague", age: "53"
         },
       :matriarch => {
-       name: "lady Montague", age: "54"}
+       name: "lady Montague", age: "54"
+        },
       :hero => {
         #your key/value pairs here
         name: "Romeo", age: "15", status: "alive"
